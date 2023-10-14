90718a6d24d75931bf13077c6ec9a307e4b78949
Henrik Lurz <nhkw0001@login02.css.lan>
2023-10-14 17:50:16 +0200

    Improved comments and added student code section

--- match_dt_task_1/match_dt_task_1/task_1a.py
+++ match_dt_task_1/match_dt_task_1/task_1a.py
@@ -3,16 +3,24 @@ import rclpy
 from rclpy.node import Node
 
 def main(args = None):
-    # Initialize ROS2 node. 
-    rclpy.init(args = args)
+    # Initialize ROS2 node and ROS2 communication (e.g. topics)
+    rclpy.init(args = args) 
 
-    task_1a_node = Node("task_1a")
+    # Create ROS2 Node. This is used to create for example a topic publisher.
+    task_1a_node = Node("task_1a")  
+
+    #############################################################################################
+    # Start of student code section
+
     task_1a_node.get_logger().info("test")
-
     
-    rclpy.spin(task_1a_node)
+    # End of student code section
+    #############################################################################################
+    
+    # Pause the programm until its killed. All tasks still will be processed in the background.
+    rclpy.spin(task_1a_node) 
 
-    # Last line
+    # Stops all communication and should always be called last in a node before finishing.
     rclpy.shutdown()
 
 if __name__ == "__main__":
