e476d0206f40a43dd0d5eaa74339d56b4686a34a
Henrik Lurz <henrik.lurz@web.de>
2023-10-18 22:50:47 +0200

    Added templates for 1b-e

---
+++ match_dt_task_1/match_dt_task_1/task_1c.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from time import sleep
+
+import rclpy
+from rclpy.node import Node
+
+#############################################################################################
+# Start of student code section
+
+
+
+# End of student code section
+#############################################################################################
+
+def main(args = None):
+    # Initialize ROS2 node and ROS2 communication (e.g. topics)
+    rclpy.init(args = args) 
+
+    # Create ROS2 Node. This is used to create for example a topic publisher.
+    task_1c_node = Node("task_1c")  
+
+    # Initial sleep to wait for everything to boot properly.
+    sleep(8)
+
+    #############################################################################################
+    # Start of student code section
+
+
+    
+    # End of student code section
+    #############################################################################################
+    
+    # Pause the programm until its killed. All tasks still will be processed in the background.
+    rclpy.spin(task_1c_node) 
+
+    # Stops all communication and should always be called last in a node before finishing.
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
